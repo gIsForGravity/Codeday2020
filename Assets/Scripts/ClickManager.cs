8f63b0607ac1741f1540446dce916e3222a9bf5d
Gravity <gavintantleff@gmail.com>
2020-12-04 20:14:10 -0500

    Start some basic tests and stuff

---
+++ Assets/Scripts/ClickManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickManager : MonoBehaviour
+{
+    private Camera _camera;
+
+    void Awake()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("click");
+            Vector3 mousePos = _camera.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null) {
+                Debug.Log(hit.collider.gameObject.name);
+                hit.transform.position = new Vector2(10, 15);
+            }
+        }
+    }
+
+}
