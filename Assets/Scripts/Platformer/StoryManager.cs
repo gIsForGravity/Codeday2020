8d49d0d83dadc4efb33aa941dafb99e6b765a5df
Gravity <gavintantleff@gmail.com>
2020-12-06 15:15:27 -0500

    Add levels, platforming, etc.

---
+++ Assets/Scripts/Platformer/StoryManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scripts.Platformer
+{
+    public class StoryManager : MonoBehaviour
+    {
+        [SerializeField] private string[] storylines;
+        [SerializeField] private Text _text;
+
+        private void Awake()
+        {
+            _text.text = storylines[GameManager.Level];
+        }
+    }
+}
